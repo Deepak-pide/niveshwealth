51fe218181ea3867e07956e4b54b5480156e4f98
Deepak Gupta <dg3667610@gmail.com>
2025-08-07 14:38:03 +0000

    so do
    To achieve this, I will take the following steps:
    
    Add PWA Depen

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -23,4 +34,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
